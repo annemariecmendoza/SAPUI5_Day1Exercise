55d252adb814acdf56c87cb605387a62adbdb4ea
Anne Marie Charmaine Mendoza <anne.marie.c.mendoza@accenture.com>
2026-05-06 06:19:45 +0000

    initial commit

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("exercise1surveyform.controller.MainView", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["exercise1surveyform/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
