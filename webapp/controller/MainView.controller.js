41acece5786fef8b409b7ef9e1074d9af6eff4e0
Anne Marie Charmaine Mendoza <anne.marie.c.mendoza@accenture.com>
2026-05-07 00:35:59 +0000

    built the form with clear button event

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -5,6 +5,21 @@ sap.ui.define([
 
     return Controller.extend("exercise1surveyform.controller.MainView", {
         onInit() {
+        },
+        onPress: function(){
+            //initiate this controller
+            var surveyForm = this.getView();
+
+            //clear the fields 
+            //formvariable[to call the form].byId[to get the field ID]("[fieldID]"").setValue[to set the value]("[value]");
+            surveyForm.byId("NameInput").setValue("");
+            surveyForm.byId("StreetNoInput").setValue("");
+            surveyForm.byId("StreetInput").setValue("");
+            surveyForm.byId("ZipCodeInput").setValue("");
+            surveyForm.byId("CityInput").setValue("");
+
+            //formvariable[to call the form].byId[to get the field ID]("[fieldID]"").setSelectedKey[to set the dropdownvalue]("[value]");
+            surveyForm.byId("CountryBox").setSelectedKey("");
         }
     });
 });
